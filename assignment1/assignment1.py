eeec291775ae19daf60293993c4e554112873e56
maxpoon <maxpanziyuan@gmail.com>
2016-12-02 00:01:19 +0800

    modify code

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -16,19 +16,19 @@ if ( len(fname) < 1 ) : fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
 	if not line.startswith('From: ') : continue
-	line = line.rstrip()
+	line_striped = line.rstrip()
 	# Find email domain using regular expression
 	#x = re.findall('^From.[A-Za-z0-9._%+-]+@([A-Za-z0-9.-]+\.[A-Za-z]{2,6})', line)
-	x = re.findall('[A-Za-z0-9._%+-]+@([A-Za-z0-9.-]+\.[A-Za-z]{2,6})', line)
+	x = re.findall('[A-Za-z0-9._%+-]+@([A-Za-z0-9.-]+\.[A-Za-z]{2,6})', line_striped)
 	if len(x) > 0:
-		domain = x[0]
-		cur.execute('SELECT count FROM Counts WHERE org = ? ', (domain, ))
+		org = x[0]
+		cur.execute('SELECT count FROM Counts WHERE org = ? ', (org, ))
 		row = cur.fetchone()
 		if row is None:
 			cur.execute('''INSERT INTO Counts (org, count)
-				 VALUES ( ?, 1 )''', ( domain, ) )
+				 VALUES ( ?, 1 )''', ( org, ) )
 		else :
-			cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?', (domain, ))
+			cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?', (org, ))
 		conn.commit()
 
 # https://www.sqlite.org/lang_select.html
